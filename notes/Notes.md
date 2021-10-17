b0d64e88e8fa0e0b3602a0e8be3a5904ac63835b
Aaron Lao <91438480+aajmlao@users.noreply.github.com>
2021-10-16 17:27:17 -0700

    10/16

--- notes/Notes.md
+++
@@ -1,6 +0,0 @@
-abstroction: hiding the complex instruction, and presents the simple solution. 
-encapsulation: 
-
-
-
-- worksheet means excel
---
+++ test/CalcutionofGPA/Program.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module Box
+    sub main(args as string())
+        dim area as igrade = new calculation()
+        area.start()
+    End sub
+End Module
